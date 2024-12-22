2ab12f922463bf9db6ed4b56af0cd15c52548823
Abdul100-han <abdulmuhd857@gmail.com>
2024-12-22 19:19:23 +0100

    implemented updating a single book with id

--- backend/index.js
+++ backend/index.js
@@ -78,6 +78,33 @@ app.get('/books/:id', async (request, response) => {
     }
 })
 
+// route for upadting a book
+app.put('/books/:id', async (request, response) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const {id} = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if(!result) {
+            return response.status(400).send({message: 'Book not found'});
+        }
+        return response.status(200).send({message: 'Books updates successfully'});
+    } catch (error) {
+        
+    }
+})
+
+
 
 
 mongoose.connect(mongoDBURL)
