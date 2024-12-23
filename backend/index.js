f8341fd1ad2890a601dd5e030ecae3841c7d058c
Abdul100-han <abdulmuhd857@gmail.com>
2024-12-23 12:13:25 +0100

    Allowed cors policy in the backend

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,25 @@ import { mongoDBURL, PORT } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModels.js';
 import booksRoute from './routes/bookRoute.js'
-
+import cors from 'cors';
 
 const app = express();
 
 // middleware for parsing request body
 app.use(express.json())
 
+// middle ware for handing cors policy
+// option 1. Allow all origin with Default of cors
+app.cors(cors());
+
+// Option 2: Allow custom origin
+app.use(
+    cors({
+        origin: '',
+        methods: [],
+        allowedHeaders: [],
+    })
+)
 
 app.get('/', (request, response) => {
     console.log(request);
