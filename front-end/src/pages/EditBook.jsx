b6924f6fb49b42dc1aaf33f5b3767323ba33dfda
Abdul100-han <abdulmuhd857@gmail.com>
2024-12-23 17:44:27 +0100

    Implemented book details display in the frontend

--- front-end/src/pages/EditBook.jsx
+++ front-end/src/pages/EditBook.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const EditBook = () => {
+
+  const param = useParams()
+
+  console.log(param);
+  
   return (
     <div>EditBook</div>
   )
